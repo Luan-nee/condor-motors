9eda157ef8f94df7153cd6e3b20d411297763d42
DiegoAlessandroMotta <diegomottaq@gmail.com>
2025-02-06 16:04:10 -0500

    setup vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    build: {
+      outDir: '../server/client-build',
+      emptyOutDir: true,
+    },
+    server: {
+      port: parseInt(process.env.VITE_PORT),
+    },
+  });
+};
