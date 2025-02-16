eae933b6f9ef0aeb53e5b249863fb4559d216020
DiegoAlessandroMotta <diegomottaq@gmail.com>
2025-02-16 13:24:47 -0500

    Add linting rules

---
+++ server/eslint.config.mjs
@@ -0,0 +1,20 @@
+import love from 'eslint-config-love'
+
+export default [
+  {
+    ...love,
+    files: ['**/*.js', '**/*.ts'],
+    ignores: [
+      '**/build/**/*',
+      '**/client-build/**/*',
+      '**/.vscode/**/*',
+      '**/node_modules/**/*'
+    ],
+    rules: {
+      ...love.rules,
+      'no-magic-numbers': 'off',
+      '@typescript-eslint/no-magic-numbers': 'off',
+      'eslint-comments/require-description': 'off'
+    }
+  }
+]
