8fdeee80d2c85f6ef6c16c91b901597dddc97f29
DiegoAlessandroMotta <diegomottaq@gmail.com>
2025-03-14 22:46:28 -0500

    Refactor: remove stock from UpdateProductoDto and update validation schema for product updates

--- server/src/domain/validators/entities/productos/producto.validator.ts
+++ server/src/domain/validators/entities/productos/producto.validator.ts
@@ -21,5 +21,21 @@ const createProductoSchema = z.object({
 export const createProductoValidator = (object: unknown) =>
   createProductoSchema.safeParse(object)
 
+const updateProductoSchema = z.object({
+  nombre: productoSchema.nombre,
+  descripcion: productoSchema.descripcion,
+  maxDiasSinReabastecer: productoSchema.maxDiasSinReabastecer,
+  stockMinimo: productoSchema.stockMinimo,
+  cantidadMinimaDescuento: productoSchema.cantidadMinimaDescuento,
+  cantidadGratisDescuento: productoSchema.cantidadGratisDescuento,
+  porcentajeDescuento: productoSchema.porcentajeDescuento,
+  colorId: productoSchema.colorId,
+  categoriaId: productoSchema.categoriaId,
+  marcaId: productoSchema.marcaId,
+  precioCompra: productoSchema.precioCompra,
+  precioVenta: productoSchema.precioVenta,
+  precioOferta: productoSchema.precioOferta
+})
+
 export const updateProductoValidator = (object: unknown) =>
-  createProductoSchema.partial().safeParse(object)
+  updateProductoSchema.partial().safeParse(object)
