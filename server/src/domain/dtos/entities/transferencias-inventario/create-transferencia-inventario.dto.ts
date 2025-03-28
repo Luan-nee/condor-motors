6e627c0a4696055f4f96b6b34ce009b4f6e4c0ed
DiegoAlessandroMotta <diegomottaq@gmail.com>
2025-03-28 08:27:20 -0500

    feat: Add validation for duplicated products in CreateTransferenciaInvDto

--- server/src/domain/dtos/entities/transferencias-inventario/create-transferencia-inventario.dto.ts
+++ server/src/domain/dtos/entities/transferencias-inventario/create-transferencia-inventario.dto.ts
@@ -15,6 +15,25 @@ export class CreateTransferenciaInvDto {
     this.items = detalleVenta
   }
 
+  private static validateDuplicatedProducts(
+    createTransferenciaInvDto: CreateTransferenciaInvDto
+  ) {
+    const productoIds = new Set<number>()
+    const duplicateProductIds = new Set<number>()
+
+    for (const { productoId } of createTransferenciaInvDto.items) {
+      if (productoIds.has(productoId)) {
+        duplicateProductIds.add(productoId)
+      } else {
+        productoIds.add(productoId)
+      }
+    }
+
+    if (duplicateProductIds.size > 0) {
+      return `Existen productos duplicados en los items: ${[...duplicateProductIds].join(', ')}`
+    }
+  }
+
   static create(input: any): [string?, CreateTransferenciaInvDto?] {
     const result = createTransferenciaInvValidator(input)
 
@@ -22,6 +41,14 @@ export class CreateTransferenciaInvDto {
       return [result.error.message, undefined]
     }
 
-    return [undefined, new CreateTransferenciaInvDto(result.data)]
+    const { data } = result
+
+    const duplicatedProductsErrorMessage = this.validateDuplicatedProducts(data)
+
+    if (duplicatedProductsErrorMessage !== undefined) {
+      return [duplicatedProductsErrorMessage, undefined]
+    }
+
+    return [undefined, new CreateTransferenciaInvDto(data)]
   }
 }
