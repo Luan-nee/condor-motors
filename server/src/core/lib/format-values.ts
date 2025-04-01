1acfb1df8274a28725a3bff10c59c5f9000dd08c
DiegoAlessandroMotta <diegomottaq@gmail.com>
2025-04-01 12:57:39 -0500

    fix: improve file name formatting by refining character replacements and trimming

--- server/src/core/lib/format-values.ts
+++ server/src/core/lib/format-values.ts
@@ -8,6 +8,6 @@ export const formatFileName = (value: string) =>
   value
     .trim()
     .replace(/[\s]+/g, '_')
-    .replace(/[^a-zA-Z0-9._-]+/g, '-')
-    .replace(/(^-|-$)/g, '')
-    .replace(/-+/g, '-')
+    .replace(/[^a-zA-Z0-9._-]/g, '')
+    .replace(/[-_]+/g, '_')
+    .replace(/(^[-_]|[-_]$)/g, '')
