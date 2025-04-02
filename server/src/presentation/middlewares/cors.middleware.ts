b23b0388073a7a3eb828d5c9c7b9bfb0fad7ea20
DiegoAlessandroMotta <diegomottaq@gmail.com>
2025-04-01 21:37:01 -0500

    Refactor: enhance CORS middleware with additional methods and headers

--- server/src/presentation/middlewares/cors.middleware.ts
+++ server/src/presentation/middlewares/cors.middleware.ts
@@ -26,6 +26,13 @@ export class CorsMiddleware {
         CustomError.corsError(undefined, CorsMiddleware.allowedOrigins),
         false
       )
-    }
+    },
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'X-Requested-With',
+      'Accept'
+    ]
   })
 }
