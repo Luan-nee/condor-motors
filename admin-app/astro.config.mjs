496343567c02b6cb693c4708d33bbfd51bf959e6
DiegoAlessandroMotta <diegomottaq@gmail.com>
2025-04-02 00:57:37 -0500

    feat: add Solid.js integration and update TypeScript configuration

--- admin-app/astro.config.mjs
+++ admin-app/astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [solidJs()],
   vite: {
     plugins: [tailwindcss()],
   },
