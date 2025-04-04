25f6d94e40977e4260b5b43997ec073ca31635af
yukio <zviamontevilca@gmail.com>
2025-04-04 11:03:13 -0500

    productos:controller

--- server/src/presentation/entities/productos/controller.ts
+++ server/src/presentation/entities/productos/controller.ts
@@ -1,6 +1,8 @@
 import { handleError } from '@/core/errors/handle.error'
 import { CustomResponse } from '@/core/responses/custom.response'
 import { NumericIdDto } from '@/domain/dtos/query-params/numeric-id.dto'
+import { QueriesDto } from '@/domain/dtos/query-params/queries.dto'
+import { GetProductosNombre } from '@/domain/use-cases/entities/productos/get-all-productos-nombre-use-case'
 import { GetByIdData } from '@/domain/use-cases/entities/productos/get-bt-id-dates-use-case'
 import type { Request, Response } from 'express'
 
@@ -25,6 +27,26 @@ export class ProductosController {
           res,
           data
         })
+      })
+      .catch((error: unknown) => {
+        handleError(error, res)
+      })
+  }
+
+  getAll = (req: Request, res: Response) => {
+    const [error, queriesDto] = QueriesDto.create(req.query)
+
+    if (error !== undefined || queriesDto === undefined) {
+      CustomResponse.badRequest({ res, error })
+      return
+    }
+
+    const getProductos = new GetProductosNombre()
+
+    getProductos
+      .execute(queriesDto)
+      .then((productos) => {
+        CustomResponse.success({ res, data: productos })
       })
       .catch((error: unknown) => {
         handleError(error, res)
