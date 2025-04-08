2ce35ceeafe4d302f5a23ea3a74498e1fedae5a4
DiegoAlessandroMotta <diegomottaq@gmail.com>
2025-04-08 11:00:09 -0500

    fix: pass PRIVATE_STORAGE_PATH to DownloadsController for improved file handling

--- server/src/presentation/downloads/routes.ts
+++ server/src/presentation/downloads/routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express'
 import { DownloadsController } from './controller'
 import { JwtAdapter } from '@/config/jwt'
+import { envs } from '@/config/envs'
 
 export class DownloadsRoutes {
   static get routes() {
     const router = Router()
 
-    const downloadsController = new DownloadsController(JwtAdapter)
+    const downloadsController = new DownloadsController(
+      JwtAdapter,
+      envs.PRIVATE_STORAGE_PATH
+    )
 
     router.get('/:filename', downloadsController.apkDesktopApp)
 
